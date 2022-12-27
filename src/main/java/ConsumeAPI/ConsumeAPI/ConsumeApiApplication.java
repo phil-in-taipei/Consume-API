dae2355a607f675ea6128992cc6306a038101447
mikalikahi <sweeney.phil@gmail.com>
2022-12-27 10:09:36 +0800

    First commit of project to practice consuming APIs in Spring

---
+++ src/main/java/ConsumeAPI/ConsumeAPI/ConsumeApiApplication.java
@@ -0,0 +1,13 @@
+package ConsumeAPI.ConsumeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ConsumeAPI/ConsumeAPI/models/Thumbnail.java
@@ -0,0 +1,15 @@
+package ConsumeAPI.ConsumeAPI.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Thumbnail {
+
+    @JsonProperty("url")
+    private String url;
+}
